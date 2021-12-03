b3d82a46d48af1bc1e4b88b0225c0ea9dbedbcc9
DrDano <danielharned@gmail.com>
2021-12-03 10:57:38 -0800

    timeVerify now is able to compare a substring to integer of moment

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,34 @@
 // display the current day at the top of the page
 // color code time blocks for past/present/future
+// create list items for each saved task with classes of list-item and col-sm-4 col-md-3
+// create function that saves all tasks in task lists to local storage
+
+var displayCurrDay = function () {
+    var currMoment = moment().format('dddd MMMM Do YYYY')
+    $("#currentDay").text(currMoment)
+}
+
+displayCurrDay()
+
+var timeVerify = function () {
+    var currHour = moment().hour();
+    console.log(currHour)
+    headerArr = $('.card-header h4');
+
+    for (let i = 0; i < headerArr.length; i++) {
+        let currHeader = headerArr[i];
+        let headerTime = currHeader.textContent;
+        let hTimeInt = headerTime.substring(0, headerTime.length - 2)
+        console.log(hTimeInt)
+        if (headerTime === currHour) {
+            $(currHeader).parent().addClass('bg-danger')
+        }
+    }
+    // var thisHeader = $("name[header"+i+"]")
+}
+
+timeVerify();
+
+var colorCodeTaskBlock = function () {
+
+}
